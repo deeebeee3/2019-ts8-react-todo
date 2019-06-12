af00883a84bf9b5e483a7076bccd5de8988dde97
Deepak Bhari <deepak.bhari1@Deepaks-MBP.lan>
2019-06-12 14:36:14 +0100

    simple form using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Fragment, useState } from "react";
 
-const App: React.FC = () => {
+export default function App(): JSX.Element {
+
+  const [value, setValue] = useState<string>('');
+  //const [value, setValue] = useState<string>(() => { return 'function that returns string' });
+  //function useState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
+
+  type FormElem = React.FormEvent<HTMLFormElement>
+
+  const handleSubmit = (e:FormElem):void => {
+    e.preventDefault();
+    setValue('');
+  }
+
+
   return (
-    <div className="App">
-      App
-    </div>
+    <Fragment>
+      <h1>Form with TS</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={value} onChange={e => setValue(e.target.value)} required />
+        <button type="submit">Add Todo</button>
+      </form>
+    </Fragment>
   );
 }
-
-export default App;
